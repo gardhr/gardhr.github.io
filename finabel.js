48af4ba6153b1601f00b930186f91013b52f6c74
gardhr <24468810+gardhr@users.noreply.github.com>
2020-11-18 22:23:19 -0600

    update

--- finabel.js
+++ finabel.js
@@ -1,46 +1,13 @@
-/*
-Copyright (c) 2020 Sebastian Garth
-
-This is free and unencumbered software released into the public domain.
-
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org>
-*/
-
 var finabel = (function () {
-  function dump(label, value, verbose) {
-    if (verbose) console.log(label, ":", value.toString(16), "\n");
-  }
-
   var hex = "0123456789abcdef";
   var lookup = new Array(256);
   for (var index = 0; index < 256; ++index)
-    lookup[index] = hex.charAt(index >> 4) + hex.charAt(index & 0xf)
+    lookup[index] = hex.charAt(index >> 4) + hex.charAt(index & 0xf);
 
-  function text_to_hex(data) {
+  function toHex(text) {
     var result = "";
-    for (var index = 0, length = data.length; index < length; ++index) {
-      var code = data.codePointAt(index);
+    for (var index = 0, limit = text.length; index < limit; ++index) {
+      var code = text.codePointAt(index);
       do {
         result += lookup[code & 0xff];
         code >>= 8;
@@ -52,14 +19,17 @@ var finabel = (function () {
   var primes = new Array(3);
 
   primes[0] = BigInt(
-    "906746488931122279923087816774254262549325961021203686871024593960273364483640837461275651353839912478276312989231907136777434371442297783809058139703704548112582549231057887119837609961275494698594028464424938139236812786325180573685365716054578878269276609138278646089652565880503149299047623725983"
-  );
-  primes[1] = BigInt(
-    "935963679945159621618108135650731602316123462844739918966791054002220621454733515962631838558167071714943415781502503512108093455147689164719674990397035764248808486754562236727013255473894080575022971540677037449750273014794528407667454650131576454015775014701175216242011377646611112897139737772263"
-  );
-  primes[2] = BigInt(
     "900868433651123753195857434154886592863492075718887214387046829406809805283361296277175990663685161530183997243896077623165157756007099732429029873106259069821886766195661979481563101826429797570890866473513531898785774896418926615059720815237664116812063491035355207065882456370964859448027182804663"
   );
+
+  primes[1] = BigInt(
+    "906746488931122279923087816774254262549325961021203686871024593960273364483640837461275651353839912478276312989231907136777434371442297783809058139703704548112582549231057887119837609961275494698594028464424938139236812786325180573685365716054578878269276609138278646089652565880503149299047623725983"
+  );
+
+  primes[2] = BigInt(
+    "935963679945159621618108135650731602316123462844739918966791054002220621454733515962631838558167071714943415781502503512108093455147689164719674990397035764248808486754562236727013255473894080575022971540677037449750273014794528407667454650131576454015775014701175216242011377646611112897139737772263"
+  );
+
   primes.sort();
 
   var minimum_digits = primes[2].toString(16).length;
@@ -68,19 +38,20 @@ var finabel = (function () {
   var B = primes[1];
   var C = primes[2];
 
-  var record_separator = "\u001e";
-  var field_separator = "\u001c";
+  var record_separator = toHex("\u001e");
+  var field_separator = toHex("\u001c");
 
   /*
  Key stretching function
 */
 
-  function stretch(text) {
-    var result = text;
+  function stretch(value) {
+    var hex = value.toString(16);
+    var buffer = hex;
     do {
-      result += record_separator + text;
-    } while (result.length < minimum_digits);
-    return BigInt("0x" + text_to_hex(result));
+      buffer += record_separator + hex;
+    } while (buffer.length < minimum_digits);
+    return BigInt("0x" + buffer);
   }
 
   /*
@@ -90,43 +61,41 @@ var finabel = (function () {
   function hash(key, salt, rounds, digits, verbose) {
     if (rounds == null) rounds = 0;
     if (digits == null) digits = 0;
-    var list = Array.isArray(key) ? key : [key];
-    list.push(salt);
+    var keys = Array.isArray(key) ? key : [key];
+    keys.push(salt);
 
     /*
  Initial construction: concatenate keys/salt
 */
 
-    var result = record_separator;
-    for (var index = 0, length = list.length; index < length; ++index) {
-      var next = list[index];
+    var merged = record_separator;
+    for (var index = 0, limit = keys.length; index < limit; ++index) {
+      var next = keys[index];
       if (next == null || next == "") continue;
-      result += next + field_separator;
+      merged += toHex(next) + field_separator;
     }
-
-    if (verbose) console.log("Merged: ", result, "\n");
 
     /*
  Compute the hash
 */
 
+    var result = BigInt("0x" + merged);
     do {
       var Q = stretch(result);
-      dump("Q", Q, verbose);
       var R = stretch((Q * A) % B);
-      dump("R", R, verbose);
       var F = (Q * R) % C;
-      dump("F", F, verbose);
-      result = F.toString(16);
+      result = F;
     } while (rounds-- > 0);
 
+    var text = result.toString(16);
+
     if (digits > 0) {
-      var length = result.length;
-      if (length > digits) return result.substr(0, digits);
-      while (length++ < digits) result += "0";
+      var length = text.length;
+      if (length > digits) return text.substr(0, digits);
+      while (length++ < digits) text += "0";
     }
 
-    return result;
+    return text;
   }
 
   return hash;
